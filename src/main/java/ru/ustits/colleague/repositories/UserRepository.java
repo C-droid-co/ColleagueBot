0d55cb9dab44a868cfbe3a0dfbfa4ab42f77219f
Ruslan Ustits <ustitsruslan@gmail.com>
2017-10-19 23:10:02 +0300

    #10: fetchAll for message and user repositories

--- src/main/java/ru/ustits/colleague/repositories/UserRepository.java
+++ src/main/java/ru/ustits/colleague/repositories/UserRepository.java
@@ -6,6 +6,9 @@ import ru.ustits.colleague.repositories.records.UserRecord;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author ustits
@@ -45,5 +48,29 @@ public class UserRepository extends BotRepository<User, UserRecord> {
       log.error(e);
     }
     return null;
+  }
+
+  public List<UserRecord> fetchAll() {
+    try {
+      return sql().query("SELECT * FROM users",
+              resultSet -> {
+                final List<UserRecord> records = new ArrayList<>();
+                while (resultSet.next()) {
+                  records.add(toRecord(resultSet));
+                }
+                return records;
+              });
+    } catch (SQLException e) {
+      log.error("Unable to fetch messages", e);
+    }
+    return Collections.emptyList();
+  }
+
+  private UserRecord toRecord(final ResultSet resultSet) throws SQLException {
+    final Long id = resultSet.getLong(1);
+    final String firstName = resultSet.getString(2);
+    final String lastName = resultSet.getString(3);
+    final String userName = resultSet.getString(4);
+    return new UserRecord(id, firstName, lastName, userName);
   }
 }
