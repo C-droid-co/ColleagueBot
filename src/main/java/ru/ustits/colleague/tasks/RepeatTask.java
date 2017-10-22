4a7a427bc2c3ce7827a9a5aa33a13d9bf84a623d
Ruslan Ustits <ustitsruslan@gmail.com>
2017-10-22 23:26:12 +0300

    #15 new RepeatTask implementation

--- src/main/java/ru/ustits/colleague/tasks/RepeatTask.java
+++ src/main/java/ru/ustits/colleague/tasks/RepeatTask.java
@@ -2,11 +2,13 @@ package ru.ustits.colleague.tasks;
 
 import lombok.extern.log4j.Log4j2;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import ru.ustits.colleague.commands.RepeatCommand;
 
 /**
  * @author ustits
@@ -14,21 +16,17 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 @Log4j2
 public class RepeatTask implements Job {
 
-  private final AbsSender absSender;
-  private final SendMessage message;
-
-  public RepeatTask(final AbsSender absSender, final SendMessage message) {
-    this.absSender = absSender;
-    this.message = message;
-  }
-
   @Override
   public void execute(final JobExecutionContext jobExecutionContext) throws JobExecutionException {
-    log.info(String.format("Repeated: %s in chat: %s", message.getText(), message.getChatId()));
+    final JobDataMap data = jobExecutionContext.getJobDetail().getJobDataMap();
+    final AbsSender sender = (AbsSender) data.get(RepeatCommand.SENDER_KEY);
+    final String message = data.getString(RepeatCommand.MESSAGE_KEY);
+    final Long chatId = data.getLong(RepeatCommand.CHAT_KEY);
     try {
-      absSender.execute(message);
+      sender.execute(new SendMessage(chatId, message));
+      log.info("Repeated: {} in chat: {}", message, chatId);
     } catch (TelegramApiException e) {
-      log.error(e);
+      log.error("Unable to send repeat message", e);
     }
   }
 }
