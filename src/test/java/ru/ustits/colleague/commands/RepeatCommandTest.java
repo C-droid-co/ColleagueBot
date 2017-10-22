7fe470518e74d6560c046ed28e81528085a036ac
Ruslan Ustits <ustitsruslan@gmail.com>
2017-10-22 22:09:50 +0300

    #15 test scheduling

--- src/test/java/ru/ustits/colleague/commands/RepeatCommandTest.java
+++ src/test/java/ru/ustits/colleague/commands/RepeatCommandTest.java
@@ -5,23 +5,30 @@ import org.junit.Test;
 import org.quartz.CronExpression;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.Trigger;
 import org.telegram.telegrambots.bots.AbsSender;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author ustits
  */
 public class RepeatCommandTest {
 
+  private static final String[] GOOD_ARGS = {"*", "1", "*", "?", "*", "*", "some", "text"};
+
   private RepeatCommand command;
+  private Scheduler scheduler;
 
   @Before
   public void setUp() throws Exception {
-    command = new RepeatCommand("random", mock(Scheduler.class));
+    scheduler = mock(Scheduler.class);
+    command = new RepeatCommand("random", scheduler);
   }
 
   @Test
@@ -33,10 +40,10 @@ public class RepeatCommandTest {
   }
 
   @Test
-  public void testParseCron() throws Exception {
-    final String[] arguments = {"*", "1", "*", "?", "*", "*", "text"};
-    final Optional<CronExpression> expression = command.parseCron(arguments);
-    assertThat(expression).isPresent();
+  public void testScheduleTask() throws Exception {
+    final boolean result = command.scheduleTask(GOOD_ARGS, mock(AbsSender.class));
+    assertThat(result).isTrue();
+    verify(scheduler).scheduleJob(any(JobDetail.class), any(Trigger.class));
   }
 
   @Test
@@ -47,16 +54,22 @@ public class RepeatCommandTest {
   }
 
   @Test
+  public void testParseCron() throws Exception {
+    final Optional<CronExpression> expression = command.parseCron(GOOD_ARGS);
+    assertThat(expression).isPresent();
+  }
+
+  @Test
   public void testParseCronWithNotValidCron() throws Exception {
-    final String[] arguments = {"*", "error", "*", "?", "*", "*", "text"};
+    final String[] arguments = GOOD_ARGS;
+    arguments[0] = "error";
     final Optional<CronExpression> expression = command.parseCron(arguments);
     assertThat(expression).isNotPresent();
   }
 
   @Test
   public void testParseMessage() throws Exception {
-    final String[] arguments = {"*", "1", "*", "?", "*", "*", "some", "text"};
-    final Optional<String> text = command.parseMessage(arguments);
+    final Optional<String> text = command.parseMessage(GOOD_ARGS);
     assertThat(text).isPresent().contains("some text");
   }
 }
