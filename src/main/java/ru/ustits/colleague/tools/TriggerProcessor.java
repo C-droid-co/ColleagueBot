c774bb07aa98354a704514364902eb2ac302be7f
Ruslan Ustits <ustitsruslan@gmail.com>
2017-11-02 21:46:51 +0300

    #26 quote string before regexp pattern compiling

--- src/main/java/ru/ustits/colleague/tools/TriggerProcessor.java
+++ src/main/java/ru/ustits/colleague/tools/TriggerProcessor.java
@@ -25,11 +25,16 @@ public class TriggerProcessor {
   }
 
   boolean hasTrigger(final String text, final String trigger) {
-    final String regexp = String.format("\\b%s\\b", trigger);
+    final String regexp = prepareRegexp(trigger);
     final Pattern pattern = Pattern.compile(regexp,
             Pattern.CASE_INSENSITIVE + Pattern.UNICODE_CASE);
     final Matcher matcher = pattern.matcher(text);
     return matcher.find();
+  }
+
+  String prepareRegexp(final String text) {
+    final String toPattern = Pattern.quote(text);
+    return String.format("\\b%s\\b", toPattern);
   }
 
   SendMessage createMessage(final String response) {
