0fd6fd768a4bba9f51c181d3cfed454fd4ee0445
Ruslan Ustits <ustitsruslan@gmail.com>
2017-11-09 22:02:42 +0300

    #21 added admin strategy

---
+++ src/main/java/ru/ustits/colleague/commands/triggers/AdminStrategy.java
@@ -0,0 +1,44 @@
+package ru.ustits.colleague.commands.triggers;
+
+import org.apache.commons.lang3.StringUtils;
+import ru.ustits.colleague.repositories.records.TriggerRecord;
+
+import java.util.Optional;
+
+/**
+ * @author ustits
+ */
+public final class AdminStrategy implements TriggerStrategy {
+
+  private final Long adminId;
+  private final UserStrategy innerStrategy;
+
+  public AdminStrategy(final Long adminId) {
+    this(adminId, new UserStrategy());
+  }
+
+  public AdminStrategy(final Long adminId, final UserStrategy innerStrategy) {
+    this.adminId = adminId;
+    this.innerStrategy = innerStrategy;
+  }
+
+  @Override
+  public TriggerRecord buildRecord(final Long userId, final Long chatId, final String[] arguments) {
+    final Optional<Long> parsedChatId = parseChatId(arguments);
+    if (parsedChatId.isPresent() && userId.equals(adminId)) {
+      return innerStrategy.buildRecord(userId, parsedChatId.get(), arguments);
+    }
+    return null;
+  }
+
+  @Override
+  public int parametersCount() {
+    return 3;
+  }
+
+  protected final Optional<Long> parseChatId(final String[] args) {
+    final Long chatId = StringUtils.isNumeric(args[1]) ? Long.parseLong(args[1]) : null;
+    return Optional.ofNullable(chatId);
+  }
+
+}
---
+++ src/test/java/ru/ustits/colleague/commands/triggers/AdminStrategyTest.java
@@ -0,0 +1,63 @@
+package ru.ustits.colleague.commands.triggers;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.ustits.colleague.repositories.records.TriggerRecord;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.ustits.colleague.RandomUtils.aLong;
+import static ru.ustits.colleague.RandomUtils.string;
+
+/**
+ * @author ustits
+ */
+public class AdminStrategyTest {
+
+  private AdminStrategy strategy;
+  private Long chatId;
+  private Long adminId;
+
+  @Before
+  public void setUp() throws Exception {
+    chatId = aLong();
+    adminId = aLong();
+    strategy = new AdminStrategy(adminId);
+  }
+
+  @Test
+  public void testBuildRecord() throws Exception {
+    final TriggerRecord record = strategy.buildRecord(adminId, aLong(), goodArgs());
+    assertThat(record.getUserId()).isEqualTo(adminId);
+    assertThat(record.getChatId()).isEqualTo(chatId);
+    assertThat(record.getMessage()).isNotEmpty();
+    assertThat(record.getTrigger()).isNotEmpty();
+  }
+
+  @Test
+  public void testBuildRecordIfUserIsNotAdmin() throws Exception {
+    final TriggerRecord record = strategy.buildRecord(aLong(), aLong(), goodArgs());
+    assertThat(record).isNull();
+  }
+
+  @Test
+  public void testParseChatId() throws Exception {
+    final Optional<Long> parsedChatId = strategy.parseChatId(goodArgs());
+    assertThat(parsedChatId).isPresent().hasValue(chatId);
+  }
+
+  @Test
+  public void testParseChatIdIfNotNumberPassed() throws Exception {
+    final Optional<Long> parsedChatId = strategy.parseChatId(badArgs());
+    assertThat(parsedChatId).isNotPresent();
+  }
+
+  private String[] goodArgs() {
+    return new String[]{ string(), String.valueOf(chatId), string() };
+  }
+
+  private String[] badArgs() {
+    return new String[]{ string(), string(), string() };
+  }
+}
