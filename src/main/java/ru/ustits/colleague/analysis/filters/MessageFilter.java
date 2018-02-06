774c3dd36349822d894f941ae67f95bf1e2e8bf9
Ruslan Ustits <ustitsruslan@gmail.com>
2018-02-06 23:27:06 +0300

    refused of TwitterFilter class to use of static method in MessageFilter

--- src/main/java/ru/ustits/colleague/analysis/filters/MessageFilter.java
+++ src/main/java/ru/ustits/colleague/analysis/filters/MessageFilter.java
@@ -1,5 +1,6 @@
 package ru.ustits.colleague.analysis.filters;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.function.Predicate;
@@ -8,13 +9,27 @@ import java.util.function.Predicate;
  * @author ustits
  */
 @RequiredArgsConstructor
-public class MessageFilter implements Predicate<String> {
+public final class MessageFilter implements Predicate<String> {
 
   private final String message;
+
+  public static MessageFilter newInstance(final MessageFilters filter) {
+    return new MessageFilter(filter.getText());
+  }
 
   @Override
   public boolean test(final String s) {
     return !s.contains(message);
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  public enum MessageFilters {
+
+    TWITTER("Посмотрите, о чем твитнул");
+
+    private final String text;
+
   }
 
 }
--- src/main/java/ru/ustits/colleague/commands/stats/WordStatsCmd.java
+++ src/main/java/ru/ustits/colleague/commands/stats/WordStatsCmd.java
@@ -7,8 +7,8 @@ import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.AbsSender;
 import ru.ustits.colleague.analysis.Cleanup;
 import ru.ustits.colleague.analysis.SimpleTokenizer;
+import ru.ustits.colleague.analysis.filters.MessageFilter;
 import ru.ustits.colleague.analysis.filters.NoUrlFilter;
-import ru.ustits.colleague.analysis.filters.TwitterFilter;
 import ru.ustits.colleague.repositories.Repository;
 import ru.ustits.colleague.repositories.records.MessageRecord;
 import ru.ustits.colleague.repositories.records.StopWordRecord;
@@ -22,6 +22,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.Integer.toUnsignedLong;
+import static ru.ustits.colleague.analysis.filters.MessageFilter.MessageFilters.TWITTER;
 import static ru.ustits.colleague.tools.ListUtils.count;
 import static ru.ustits.colleague.tools.MapUtils.limit;
 import static ru.ustits.colleague.tools.MapUtils.sortByValue;
@@ -59,7 +60,7 @@ public final class WordStatsCmd extends StatsCommand {
     final List<MessageRecord> messages = getService().messagesForUser(userId, chatId);
     final List<String> tokens = tokenizer.tokenize(messages.stream()
             .map(MessageRecord::getText)
-            .filter(new TwitterFilter())
+            .filter(MessageFilter.newInstance(TWITTER))
             .filter(new NoUrlFilter())
             .collect(Collectors.toList()));
 
