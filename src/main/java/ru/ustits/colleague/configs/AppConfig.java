a04d55f02c3d4cda3075bbdee2d49c5b7d030fb8
Ruslan Ustits <ustitsruslan@gmail.com>
2018-02-24 19:17:52 +0300

    #58 hibernate configuration

--- src/main/java/ru/ustits/colleague/configs/AppConfig.java
+++ src/main/java/ru/ustits/colleague/configs/AppConfig.java
@@ -1,6 +1,7 @@
 package ru.ustits.colleague.configs;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -11,14 +12,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import ru.ustits.colleague.tasks.RepeatScheduler;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author ustits
  */
 @Configuration
+@EnableJpaRepositories(basePackages = "ru.ustits.colleague.repositories")
 @ComponentScan(basePackages = "ru.ustits.colleague")
 @PropertySource("classpath:bot_config.properties")
 public class AppConfig {
@@ -27,6 +37,9 @@ public class AppConfig {
 
   @Autowired
   private Environment env;
+
+  @Autowired
+  private ResourceLoader loader;
 
   @Bean
   public RepeatScheduler scheduler() throws SchedulerException {
@@ -49,6 +62,30 @@ public class AppConfig {
     dataSource.setPassword(env.getRequiredProperty("db.password"));
     dataSource.setPortNumber(Integer.valueOf(env.getRequiredProperty("db.port")));
     return dataSource;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(final DataSource dataSource) {
+    final LocalContainerEntityManagerFactoryBean manager = new LocalContainerEntityManagerFactoryBean();
+    manager.setDataSource(dataSource);
+    manager.setPackagesToScan("ru.ustits.colleague.repositories.records");
+    manager.setPersistenceProvider(new HibernatePersistenceProvider());
+    final Resource resource = loader.getResource("classpath:hibernate.properties");
+    final Properties properties = new Properties();
+    try (final InputStream data = resource.getInputStream()) {
+      properties.load(data);
+    } catch (IOException e) {
+      throw new IllegalStateException("Unable to read hibernate properties");
+    }
+    manager.setJpaProperties(properties);
+    return manager;
+  }
+
+  @Bean
+  public JpaTransactionManager transactionManager(final DataSource dataSource) {
+    final JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setDataSource(dataSource);
+    return transactionManager;
   }
 
   @Bean
