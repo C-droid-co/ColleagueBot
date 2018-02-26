da110af98d48c95f1fb8a945d6cb6020aabdddbd
Ruslan Ustits <ustitsruslan@gmail.com>
2018-02-26 17:35:25 +0300

    #48 added caching for often used and rarely updated repositories

--- src/main/java/ru/ustits/colleague/repositories/ChatStateRepository.java
+++ src/main/java/ru/ustits/colleague/repositories/ChatStateRepository.java
@@ -1,16 +1,11 @@
 package ru.ustits.colleague.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import ru.ustits.colleague.repositories.records.ChatRecord;
 import ru.ustits.colleague.repositories.records.ChatStateRecord;
-
-import java.util.Optional;
 
 /**
  * @author ustits
  */
 public interface ChatStateRepository extends CrudRepository<ChatStateRecord, Integer> {
-
-  Optional<ChatStateRecord> findByChatId(final ChatRecord chatRecord);
 
 }
--- src/main/java/ru/ustits/colleague/repositories/ChatsRepository.java
+++ src/main/java/ru/ustits/colleague/repositories/ChatsRepository.java
@@ -1,11 +1,27 @@
 package ru.ustits.colleague.repositories;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import ru.ustits.colleague.repositories.records.ChatRecord;
+
+import java.util.Optional;
 
 /**
  * @author ustits
  */
 public interface ChatsRepository extends CrudRepository<ChatRecord, Long> {
+
+  @Override
+  @CacheEvict(allEntries = true, cacheNames = {"chats", "chats_exists"})
+  <S extends ChatRecord> S save(final S entity);
+
+  @Override
+  @Cacheable(cacheNames = "chats")
+  Optional<ChatRecord> findById(final Long aLong);
+
+  @Override
+  @Cacheable(cacheNames = "chats_exists")
+  boolean existsById(final Long aLong);
 
 }
